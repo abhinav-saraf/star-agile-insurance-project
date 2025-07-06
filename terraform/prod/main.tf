1b21fe4eed09f33745508cd60c540d4001e09342
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-06 20:23:31 +0530

    Update main.tf

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "app_server" {
  provisioner "local-exec" {
   command = <<EOT
     echo "[prod]
-    ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'" > /var/lib/jenkins/workspace/InsureMe/ansible/inventory/prod
+    ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'" > /var/lib/jenkins/workspace/InsureMe/ansible/prod
   EOT
 }
 
