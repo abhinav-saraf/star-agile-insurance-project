072205ad7601090dabb6d91f644f31cdb97f3d82
Abhinav Saraf <99379559+abhinav-saraf@users.noreply.github.com>
2025-07-06 20:22:34 +0530

    Update main.tf

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -2,32 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "test" {
-  ami           = "ami-020cba7c55df1f615"
-  instance_type = "t2.micro"
-  key_name      = "id_rsa"
+resource "aws_instance" "app_server" {
+  ami                    = "ami-020cba7c55df1f615"
+  instance_type          = "t2.micro"
+  key_name               = "id_rsa"
   associate_public_ip_address = true
-
   tags = {
     Name = "InsureMe-Test-Server"
   }
 
-  provisioner "remote-exec" {
-    command = <<EOT
-      echo "[test]
-      ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'" > /var/lib/jenkins/workspace/FinanceMe/ansible/inventory/test
-    EOT
-  }
-    inline = [
-      "sudo apt update",
-      "sudo apt install docker.io -y"
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("~/.ssh/id_rsa")
-      host        = self.public_ip
-    }
+ provisioner "local-exec" {
+  command = <<EOT
+    echo "[test]
+    ${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/var/lib/jenkins/.ssh/id_rsa ansible_ssh_common_args='-o StrictHostKeyChecking=no'" > /var/lib/jenkins/workspace/InsureMe/ansible/inventory/test
+  EOT
+}
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("/var/lib/jenkins/.ssh/id_rsa")
+    host        = self.public_ip
   }
 }
 
